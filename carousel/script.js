518e52c635b8923738a3a4052030748b29215453
renztp <tullaorenz@gmail.com>
2017-10-26 13:00:28 +0800

    finished with no bugs

--- carousel/script.js
+++ carousel/script.js
@@ -9,13 +9,16 @@ var image_pos = [0, -600, -1200, -1800];
 function goes(thePosition){
 	theImg.style.left = image_pos[thePosition] + 'px';
 	x = thePosition;
-	activeLight(thePosition);
+	activeLight(x);
 }
 
 function activeLight(active){
-	for(i = 0; i < 5; i++){
+	for(i = 0; i < 4; i++){
 		document.getElementById('butt_' + active).style.color = '#fff';	
 		document.getElementById('butt_' + i).style.color = 'gray';
+		if(i == 3){
+			document.getElementById('butt_' + active).style.color = '#fff';
+		}
 	}
 }
 
@@ -24,7 +27,7 @@ function moves(direction){
 		x++;
 		theImg.style.left = image_pos[x] + 'px';
 		console.log(x);
-		if(x > 3){
+		if(x >= 4){
 			x = 0;
 			theImg.style.left = image_pos[x] + 'px';	
 		}	
@@ -47,4 +50,3 @@ function moves(direction){
 	};
 	window.setInterval(callback, 3000);
 })();
-
